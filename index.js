f7c73b9e532036635427d53e12ec9c9a0e6e72bc
Orrin19 <67312910+Orrin19@users.noreply.github.com>
2021-03-14 15:50:59 +0700

    kekw

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-class Side {
+class Army {
     constructor(inf, cav, art, cw) {
         this.branches = {};
         this.branches.infantry = {};
@@ -10,31 +10,45 @@ class Side {
         this.branches.artillery.total = art;
         this.combatWidth = cw;
     }
+    // distribution of total troopers to regiments 
+    distribute() {
+        // infantry distribution
+        this.branches.infantry.filled = Math.floor(this.branches.infantry.total / 1000);
+        this.branches.infantry.regiments = [];
+        for (let i = 0; i < this.branches.infantry.filled; i++) {
+            this.branches.infantry.regiments.push(1000);
+        }
+        this.branches.infantry.regiments.push(this.branches.infantry.total - (1000 * this.branches.infantry.filled));
+
+        // cavalry distribution
+        this.branches.cavalry.filled = Math.floor(this.branches.cavalry.total / 1000);
+        this.branches.cavalry.regiments = [];
+        for (let i = 0; i < this.branches.cavalry.filled; i++) {
+            this.branches.cavalry.regiments.push(1000);
+        }
+        this.branches.cavalry.regiments.push(this.branches.cavalry.total - (1000 * this.branches.cavalry.filled));
+
+        // artillery distribution
+        this.branches.artillery.filled = Math.floor(this.branches.artillery.total / 1000);
+        this.branches.artillery.regiments = [];
+        for (let i = 0; i < this.branches.artillery.filled; i++) {
+            this.branches.artillery.regiments.push(1000);
+        }
+        this.branches.artillery.regiments.push(this.branches.artillery.total - (1000 * this.branches.artillery.filled));
+    }
 }
 
 const arrangement = (attack, defense) => {
-    attack.branches.infantry.filled = Math.floor(attack.branches.infantry.total / 1000);
-    attack.branches.infantry.regiments = [];
-    for (let i = 0; i < attack.branches.infantry.filled; i++) {
-        attack.branches.infantry.regiments.push(1000);
-    }
-    attack.branches.infantry.regiments.push(attack.branches.infantry.total - (1000 * attack.branches.infantry.filled));
+    // distribution of total troopers to regiments 
+    attack.distribute();
+    defense.distribute();
 
-    attack.branches.cavalry.filled = Math.floor(attack.branches.cavalry.total / 1000);
-    attack.branches.cavalry.regiments = [];
-    for (let i = 0; i < attack.branches.cavalry.filled; i++) {
-        attack.branches.cavalry.regiments.push(1000);
-    }
-    attack.branches.cavalry.regiments.push(attack.branches.cavalry.total - (1000 * attack.branches.cavalry.filled));
-
-    attack.branches.artillery.filled = Math.floor(attack.branches.artillery.total / 1000);
-    attack.branches.artillery.regiments = [];
-    for (let i = 0; i < attack.branches.artillery.filled; i++) {
-        attack.branches.artillery.regiments.push(1000);
-    }
-    attack.branches.artillery.regiments.push(attack.branches.artillery.total - (1000 * attack.branches.artillery.filled));
-
-    console.log(attack.branches.infantry.regiments + ' ' + attack.branches.cavalry.regiments + ' ' + attack.branches.artillery.regiments);
+    console.log('Attacking army infantry: ' + attack.branches.infantry.total + ' => ' + attack.branches.infantry.regiments.join(', '));
+    console.log('Attacking army cavalry: ' + attack.branches.cavalry.total + ' => ' + attack.branches.cavalry.regiments.join(', '));
+    console.log('Attacking army artillery: ' + attack.branches.artillery.total + ' => ' + attack.branches.artillery.regiments.join(', '));
+    console.log('Defending army infantry: ' + defense.branches.infantry.total + ' => ' + defense.branches.infantry.regiments.join(', '));
+    console.log('Defending army cavalry: ' + defense.branches.cavalry.total + ' => ' + defense.branches.cavalry.regiments.join(', '));
+    console.log('Defending army artillery: ' + defense.branches.artillery.total + ' => ' + defense.branches.artillery.regiments.join(', '));
 }
 
 const battle = (attack, defense) => {
@@ -45,8 +59,8 @@ const battle = (attack, defense) => {
 
 
 
-let attack = new Side(4900, 1200, 2400, 15);
-let defense = new Side(4200, 1500, 3200, 15);
+let attack = new Army(4900, 1200, 2400, 15);
+let defense = new Army(4200, 1500, 3200, 15);
 
 
 battle(attack, defense);
